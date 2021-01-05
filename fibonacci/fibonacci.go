d304d7d6bf30ede812c07f7160e18352272031de
cristianekoester <cristiane.koester@mercadolivre.com>
2021-01-05 19:33:32 -0300

    update gitignore file and removing one if condition

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -2,12 +2,8 @@ package fibonacci
 
 // Fibonacci calculates the number in fibonacci sequence in position n
 func Fibonacci(n int) int {
-	if n == 0 {
-		return 0
-	}
-
-	if n == 1 {
-		return 1
+	if n == 0 || n == 1 {
+		return n
 	}
 
 	return Fibonacci(n-1) + Fibonacci(n-2)
